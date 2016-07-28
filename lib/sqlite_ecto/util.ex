f8536eb318e6956c1f4022061ee441d9a57ead00
Brett N. DiFrischia <Brett.DiFrischia@thomsonreuters.com>
2016-07-28 13:21:53 -0500

    Dynamically determine random module.  May want to move this to compile time.

--- lib/sqlite_ecto/util.ex
+++ lib/sqlite_ecto/util.ex
@@ -2,12 +2,14 @@ defmodule Sqlite.Ecto.Util do
   @moduledoc "Common utilties used by multiple Sqlite.Ecto modules."
 
 
-  @otp_maj_vrs  :erlang.system_info(:otp_release)
-                |> Enum.take_while(&(&1 != ?.))
-                |> to_string
-                |> Integer.parse
-                |> elem(0)
-  @rand_mod if @otp_maj_vrs >= 19, do: :rand, else: :random
+  defp otp_release do
+    :erlang.system_info(:otp_release)
+    |> to_string
+    |> Integer.parse
+    |> elem(0)
+  end
+
+  def rand_mod, do: if otp_release >= 19, do: :rand, else: :random
 
   # Use Ecto's JSON library (currently Poison) for embedded JSON datatypes.
   def json_library, do: Application.get_env(:ecto, :json_library)
@@ -22,7 +24,7 @@ defmodule Sqlite.Ecto.Util do
   end
 
   # Generate a random string.
-  def random_id, do: @rand_mod.uniform |> Float.to_string |> String.slice(2..10)
+  def random_id, do: rand_mod.uniform |> Float.to_string |> String.slice(2..10)
 
   # Quote the given identifier.
   def quote_id({nil, id}), do: quote_id(id)
